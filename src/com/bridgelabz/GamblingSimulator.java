20b951070881a7f0f105f8eb0ddc74698939b933
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-25 15:24:51 +0530

    Initial Stake and Bet Amount

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -4,5 +4,8 @@ public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("***| Gambling Simulator Program |***");
 
+        int initialStake = 100;
+        int betAmount = 1;
+        System.out.println("Initial Stake = " + initialStake + "\t||\tBet Amount = " + betAmount);
     }
 }
