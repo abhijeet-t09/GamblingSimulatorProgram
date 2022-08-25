6ac42cbd21ed19b5171c889c23cf8dfd7a569ed9
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-25 15:07:11 +0530

    First Commit

---
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println("***| Gambling Simulator Program |***");
+
+    }
+}
