cc45d112657d5db3169d59aa9c5a1f67c091628e
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-25 15:32:13 +0530

    First Gamble: Win or Lose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -6,6 +6,17 @@ public class GamblingSimulator {
 
         int initialStake = 100;
         int betAmount = 1;
-        System.out.println("Initial Stake = " + initialStake + "\t||\tBet Amount = " + betAmount);
+        int win = 1;
+
+        System.out.println("\nInitial Stake = " + initialStake + "\t||\tBet Amount = " + betAmount);
+
+        int roll = (int)(Math.random() * 10) % 2;
+
+        System.out.println();
+        if (roll == win){
+            System.out.println("You Won !!");
+        }else {
+            System.out.println("You Lost !!");
+        }
     }
 }
