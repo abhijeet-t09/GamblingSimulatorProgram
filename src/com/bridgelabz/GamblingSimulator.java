998b10e635f09faae69b1134b5ba2a861c62b47e
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-25 18:21:37 +0530

    Amount after 20 Days of Play

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -7,21 +7,25 @@ public class GamblingSimulator {
         int initialStake = 100;
         int betAmount = 1;
         int win = 1;
-        int currentAmount = initialStake;
+        int currentAmount = 0;
+        int days = 20;
 
         System.out.println("\nInitial Stake = " + initialStake + "\t||\tBet Amount = " + betAmount);
 
         System.out.println();
 
-        while(currentAmount < 150 && currentAmount > 50){
-            int roll = (int)(Math.random() * 10) % 2;
-            if (roll == win){
-                System.out.println("You Won !!");
-                currentAmount = currentAmount + betAmount;
-            }else {
-                System.out.println("You Lost !!");
-                currentAmount = currentAmount - betAmount;
+        for (int i = 0; i < days; i++){
+            while(initialStake < 150 && initialStake > 50){
+                int roll = (int)(Math.random() * 10) % 2;
+                if (roll == win){
+                    System.out.println("You Won !!");
+                    initialStake = initialStake + betAmount;
+                }else {
+                    System.out.println("You Lost !!");
+                    initialStake = initialStake - betAmount;
+                }
             }
+            currentAmount = currentAmount + initialStake;
         }
         System.out.println("Final Amount = " + currentAmount);
     }
